e844a5319e2861707bb5bf5861e07f7e98de8dfb
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-13 07:56:46 +0100

    Implement email validation

--- models/user.js
+++ models/user.js
@@ -8,7 +8,11 @@ const userSchema = mongoose.Schema({
     maxlength: 50,
   },
   password: String,
-  mail: String,
+  mail: {
+    type: String,
+    required: true,
+    match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
