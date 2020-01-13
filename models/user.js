72cc67863bedb36b9289ae64e3b9241e5ef30611
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-13 09:02:43 +0100

    Implement phone number validation

--- models/user.js
+++ models/user.js
@@ -17,6 +17,10 @@ const userSchema = mongoose.Schema({
     required: true,
     match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   },
+  phoneNumber: {
+    type: String,
+    match: /^((\+)33|0)[1-9](\d{2}){4}$/,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
