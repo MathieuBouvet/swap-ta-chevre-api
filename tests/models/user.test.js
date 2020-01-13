d67fede63a9395fb372a6c3a4eaee07069805d2a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-13 07:19:59 +0100

    Add username validation test

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -1,4 +1,5 @@
 const User = require("../../models/user.js");
+const mongoose = require("mongoose");
 
 describe("User model", () => {
   describe("User fields", () => {
@@ -15,6 +16,31 @@ describe("User model", () => {
     });
     it("should have a mail adress", () => {
       expect(testUser.mail).toBeDefined();
+    });
+  });
+  describe("username validation", () => {
+    const testUser = new User({
+      username: undefined,
+      password: "test",
+      mail: "test",
+    });
+    it("should not allow undefined", () => {
+      expect(testUser.validateSync()).toBeInstanceOf(
+        mongoose.Error.ValidationError
+      );
+    });
+    it("should not allow a length < 8", () => {
+      testUser.username = "short";
+      expect(testUser.validateSync()).toBeInstanceOf(
+        mongoose.Error.ValidationError
+      );
+    });
+    it("should not allow a length > 50", () => {
+      testUser.username =
+        "looooooooooooooooooooooooooooooooooooooooooooog username";
+      expect(testUser.validateSync()).toBeInstanceOf(
+        mongoose.Error.ValidationError
+      );
     });
   });
 });
