2f0c96839219ea40b7031965af368b54cd3b9702
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-15 07:01:05 +0100

    Improve user validation test isolation
    Validate only on the concerned path, dummy

--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -19,20 +19,20 @@ describe("User model", () => {
     const testUser = getDefaultUser();
     it("should not allow undefined value", () => {
       testUser.username = undefined;
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("username")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
     it("should not allow a length < 8", () => {
       testUser.username = "short";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("username")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
     it("should not allow a length > 50", () => {
       testUser.username =
         "looooooooooooooooooooooooooooooooooooooooooooog username";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("username")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
@@ -41,13 +41,13 @@ describe("User model", () => {
     const testUser = getDefaultUser();
     it("should not allow undefined value", () => {
       testUser.mail = undefined;
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("mail")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
     it("should not allow invalid email", () => {
       testUser.mail = "invalidmail";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("mail")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
@@ -56,19 +56,19 @@ describe("User model", () => {
     const testUser = getDefaultUser();
     it("should not allow undefined value", () => {
       testUser.password = undefined;
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("password")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
     it("should not allow empty value", () => {
       testUser.password = "";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("password")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
     it("should not allow a password length < 8", () => {
       testUser.password = "short";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("password")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
@@ -77,7 +77,7 @@ describe("User model", () => {
     const testUser = getDefaultUser();
     it("should not allow invalid phone numbers", () => {
       testUser.phoneNumber = "45rt5";
-      expect(testUser.validateSync()).toBeInstanceOf(
+      expect(testUser.validateSync("phoneNumber")).toBeInstanceOf(
         mongoose.Error.ValidationError
       );
     });
