ced94efdd7e4c68ec010ae3408425eb26eb4c231
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-01-24 07:05:23 +0100

    Implement InvalidArgument handling

--- services/user.service.js
+++ services/user.service.js
@@ -1,3 +1,12 @@
+const InvalidArgumentError = require("../utils/errors/InvalidArgumentError");
 exports.createUser = async function(userData) {
-  console.log(userData);
+  if (
+    typeof userData !== "object" ||
+    userData === null ||
+    Array.isArray(userData)
+  ) {
+    throw new InvalidArgumentError(
+      "userdata parameter should be an object describing a user."
+    );
+  }
 };
