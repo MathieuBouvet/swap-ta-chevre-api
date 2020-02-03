b09df2ac9b77aa559a1e1ae7d7ba24553110317d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-02-03 12:30:38 +0100

    Move mongoose connection to server
    Allow supertest to connect to mongodb-in-memory-server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const http = require("http");
 const app = require("./app");
 
@@ -36,6 +37,32 @@ const errorHandler = error => {
       throw error;
   }
 };
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Atlas!");
+  })
+  .catch(error => {
+    console.log("Unable to connect to MongoDB Atlas!");
+    console.error(error);
+  });
+
+mongoose.connection.on("disconnected", function() {
+  console.log("Mongoose default connection is disconnected");
+});
+
+process.on("SIGINT", function() {
+  mongoose.connection.close(function() {
+    console.log(
+      "Mongoose default connection is disconnected due to application termination"
+    );
+    process.exit(0);
+  });
+});
 
 const server = http.createServer(app);
 
