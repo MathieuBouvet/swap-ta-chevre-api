43edcd332fe16afd02ff8771e33332236a3d25ec
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-02-03 11:47:59 +0100

    Set correct connection string for mongoose

--- tests/dbUtils.js
+++ tests/dbUtils.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const setup = async () => {
   await mongoose.connect(
-    process.env.MONGO_URL,
+    global.MONGO_URI,
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     err => {
       if (err) {
