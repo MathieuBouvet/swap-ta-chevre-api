4151a4578d725eee320f51e244c6a2e419d4c64e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-19 10:45:47 +0100

    Implement error dispatcher wrapper

--- utils/withErrorDispatcherToExpress.js
+++ utils/withErrorDispatcherToExpress.js
@@ -1,2 +1,16 @@
+const InvalidArgumentError = require("./errors/InvalidArgumentError");
+
 module.exports = controllerMethod => {
+  if (typeof controllerMethod !== "function") {
+    throw new InvalidArgumentError(
+      `argument "controllerMethod" must be of type "function"`
+    );
+  }
+  return async (req, res, next) => {
+    try {
+      await controllerMethod(req, res);
+    } catch (err) {
+      next(err);
+    }
+  };
 };
