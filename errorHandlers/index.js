cd8379cbf89789e7d9c0dec1477ebd9f2b36b207
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-20 14:14:25 +0100

    Create error handlers mechanics

---
+++ errorHandlers/index.js
@@ -0,0 +1,13 @@
+const registeredSpecificHandlers = [];
+
+const toErrorHandlerMiddleware = specificErrorHandler => {
+  return (err, req, res, next) => {
+    if (err instanceof specificErrorHandler.handledErrorType) {
+      specificErrorHandler.handler(err, req, res, next);
+    } else {
+      next(err);
+    }
+  };
+};
+
+module.exports = registeredSpecificHandlers.map(toErrorHandlerMiddleware);
