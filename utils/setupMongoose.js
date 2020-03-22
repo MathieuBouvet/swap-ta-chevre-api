3d5e5f4dd1cb331ef076d88fb32bb7e53166237a
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-22 12:30:55 +0100

    Enable async mongooseSetup and customize db name
    It is supposed to be used when testing. It allows for waiting for an
    established connections with db and for creating different db for each
    test files. Use in beforeAll.

--- utils/setupMongoose.js
+++ utils/setupMongoose.js
@@ -1,24 +1,11 @@
 const mongoose = require("mongoose");
-module.exports = () => {
-  mongoose
-    .connect(
-      process.env.NODE_ENV === "test"
-        ? process.env.MONGO_URI_FOR_TESTS
-        : process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-      }
-    )
-    .then(() => {
-      console.log("Successfully connected to MongoDB Atlas!");
-    })
-    .catch(error => {
-      console.log("Unable to connect to MongoDB Atlas!");
-      console.error(error);
-    });
-
+const getMongoDbConnectionString = dbName => {
+  if (dbName === "") {
+    return process.env.MONGO_URI_DEFAULT;
+  }
+  return process.env.MONGO_URI_CUSTOM.replace("{customizable}", dbName + "-");
+};
+module.exports = async (dbName = "") => {
   mongoose.connection.on("disconnected", function() {
     console.log("Mongoose default connection is disconnected");
   });
@@ -31,4 +18,16 @@ module.exports = () => {
       process.exit(0);
     });
   });
+
+  try {
+    await mongoose.connect(getMongoDbConnectionString(dbName), {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Successfully connected to MongoDB Atlas ${dbName}`);
+  } catch (err) {
+    console.log("Unable to connect to MongoDB Atlas");
+    console.error(err);
+  }
 };
