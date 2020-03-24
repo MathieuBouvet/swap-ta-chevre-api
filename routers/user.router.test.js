4ee68dd0109862c56ae4cabbabeba059fd19d8d4
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-24 14:21:01 +0100

    Add tests for bad requests

--- routers/user.router.test.js
+++ routers/user.router.test.js
@@ -23,4 +23,32 @@ describe("POST /users endpoint", () => {
     expect(res.type).toBe("application/json");
     expect(res.statusCode).toBe(201);
   });
+
+  it("should respond correctly to inputs failing validations", async () => {
+    const res = await request.post("/users").send({
+      username: "bad",
+      password: "bad",
+      mail: "bad",
+      phoneNumber: "bad",
+    });
+    expect(res.status).toBe(400);
+    // todo check for correct error object in response body
+  });
+
+  it("should respond correctly to a duplicate username", async () => {
+    await request.post("/users").send({
+      username: "test-posting-user",
+      password: "thepassword",
+      mail: "test-posting-user@testmail.com",
+      phoneNumber: "0945124578",
+    });
+    const res = await request.post("/users").send({
+      username: "test-posting-user",
+      password: "thepassword",
+      mail: "test-posting-user@testmail.com",
+      phoneNumber: "0945124578",
+    });
+    expect(res.status).toBe(400);
+    // todo cehck for correct error object in response body
+  });
 });
