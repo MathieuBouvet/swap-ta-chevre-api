c6448e7f2caad443677fbb0db5ec2b4f940bf804
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-26 15:50:46 +0100

    Use the error dispatcher

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
+const errorDispatcher = require("../utils/withErrorDispatcherToExpress");
 const userService = require("../services/user.service");
 
-exports.addUser = async (req, res) => {
+exports.addUser = errorDispatcher(async (req, res) => {
   await userService.createUser(req.body);
   res.status(201).json();
-};
+});
