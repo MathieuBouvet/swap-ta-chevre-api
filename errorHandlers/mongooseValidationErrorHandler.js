9e39b612959b504327031ccbb95ca6263f7e30c0
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-27 13:27:36 +0100

    Return a correct response for validation errors
    A correct response error according to the specs

--- errorHandlers/mongooseValidationErrorHandler.js
+++ errorHandlers/mongooseValidationErrorHandler.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 module.exports = {
   handledErrorType: mongoose.Error.ValidationError,
   handler: (err, req, res) => {
-    res.status(400).json(err.errors);
+    res.status(400).json({
+      httpStatus: 400,
+      httpMessage: "Bad Request",
+      errorDetails: err.errors,
+    });
   },
 };
