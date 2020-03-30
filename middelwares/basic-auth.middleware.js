00252ca7c186d1f4eddbe8e1108b9c84c0a48bee
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-30 18:34:29 +0200

    Throw an error when authentication fails

--- middelwares/basic-auth.middleware.js
+++ middelwares/basic-auth.middleware.js
@@ -1,5 +1,15 @@
 const passport = require("passport");
+const Http401 = require("../utils/errors/Http401");
 
-module.exports = function() {
-  return passport.authenticate("local", { session: false });
+module.exports = (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(new Http401(info));
+    }
+    req.user = user;
+    return next();
+  })(req, res, next);
 };
