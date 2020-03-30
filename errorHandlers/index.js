9a1a67b16adcdba7df2a1849f2bf811dc7cd8388
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-30 18:33:22 +0200

    Add Http401 handler

--- errorHandlers/index.js
+++ errorHandlers/index.js
@@ -1,5 +1,10 @@
 const mongooseValidationErrorHandler = require("./mongooseValidationErrorHandler");
-const registeredSpecificHandlers = [mongooseValidationErrorHandler];
+const http401Handler = require("./http401Handler");
+
+const registeredSpecificHandlers = [
+  mongooseValidationErrorHandler,
+  http401Handler,
+];
 
 const toErrorHandlerMiddleware = specificErrorHandler => {
   return (err, req, res, next) => {
