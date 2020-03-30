42ea9c112f7cd5318cd90527ceebd5fa90a4825e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-30 14:27:24 +0200

    Create login endpoint stub

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,3 +5,7 @@ exports.addUser = errorDispatcher(async (req, res) => {
   await userService.createUser(req.body);
   res.status(201).json();
 });
+
+exports.login = (req, res) => {
+  res.status(501).json({ message: "Not Implemented" });
+};
--- routers/user.router.js
+++ routers/user.router.js
@@ -2,4 +2,5 @@ const router = require("express").Router();
 const controller = require("../controllers/user.controller");
 
 router.post("/", controller.addUser);
+router.post("/login", controller.login);
 module.exports = router;
