1e47e862eee2a873525a6c3230963010c15cb0d5
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-03-30 18:37:09 +0200

    Add basic authentication to login route

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const controller = require("../controllers/user.controller");
+const basicAuthentication = require("../middelwares/basic-auth.middleware");
 
 router.post("/", controller.addUser);
-router.post("/login", controller.login);
+router.post("/login", basicAuthentication, controller.login);
 module.exports = router;
