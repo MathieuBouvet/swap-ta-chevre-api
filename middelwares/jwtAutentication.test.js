249ba3c8047f98b97260fc6645d6063705f0e547
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-04 14:26:20 +0200

    Start testing jwtAuthentication middleware

---
+++ middelwares/jwtAutentication.test.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const jwtAuth = require("./jwtAutentication");
+const passport = require("passport");
+const jwtStrategy = require("../utils/passport-strategies/jwt.strategy");
+
+const mockRequest = (data, notExpired = true) => {
+  const expiration = Math.floor(Date.now() / 1000) + (notExpired ? 60 : -60);
+  return {
+    cookies: {
+      accessToken: jwt.sign(
+        { ...data, exp: expiration },
+        process.env.JWT_SECRET_KEY
+      ),
+    },
+  };
+};
+const mockResponse = () => {};
+const mockNext = () => jest.fn();
+
+passport.use(jwtStrategy);
+
+describe("Jwt autentication middleware", () => {
+  it("should attach a valid access token to the req object", (done) => {
+    const req = mockRequest({ sub: "me" });
+    const res = mockResponse();
+    const next = mockNext();
+    jwtAuth(req, res, next);
+    expect(next).toHaveBeenCalled();
+    done();
+  });
+});
