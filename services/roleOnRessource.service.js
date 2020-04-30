e08ad2033c9427961e1693e408f50bc805e4d59f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-30 14:46:17 +0200

    Implement admin role path

--- services/roleOnRessource.service.js
+++ services/roleOnRessource.service.js
@@ -1,11 +1,14 @@
 const {
-  roles: { ANON },
+  roles: { ADMIN, ANON },
 } = require("../utils/roles");
 
 module.exports = function roleOnRessource(user) {
   return (ressourceName) => (ressource) => {
     if (!user || !user.role) {
       return ANON;
+    }
+    if (user.role === ADMIN) {
+      return ADMIN;
     }
   };
 };
