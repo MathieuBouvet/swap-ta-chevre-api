550ecd1fa987e403c83305c5b39c23f36e0da194
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-04-30 14:38:10 +0200

    Implement user is anonymous path

--- services/roleOnRessource.service.js
+++ services/roleOnRessource.service.js
@@ -1,3 +1,11 @@
-module.exports = function roleOnRessource() {
-  return null;
+const {
+  roles: { ANON },
+} = require("../utils/roles");
+
+module.exports = function roleOnRessource(user) {
+  return (ressourceName) => (ressource) => {
+    if (!user || !user.role) {
+      return ANON;
+    }
+  };
 };
