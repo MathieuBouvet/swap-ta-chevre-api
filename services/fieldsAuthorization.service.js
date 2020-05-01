793ae9bd6dd389a7d1fc9e0e32ee7ad8941b6c75
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-01 16:42:24 +0200

    Throw error for unconfigured ressource name

--- services/fieldsAuthorization.service.js
+++ services/fieldsAuthorization.service.js
@@ -1,11 +1,18 @@
 module.exports = function fieldsAutorization(config) {
-  return (ressourceName) => (role) => (mode) => (ressource) => {
-    const filteredData = {};
-    for (let field in ressource) {
-      if (config[ressourceName][field][mode].includes(role)) {
-        filteredData[field] = ressource[field];
-      }
+  return (ressourceName) => {
+    if (config[ressourceName] == null) {
+      throw new Error(
+        `No configuration found for ressource '${ressourceName}'`
+      );
     }
-    return filteredData;
+    return (role) => (mode) => (ressource) => {
+      const filteredData = {};
+      for (let field in ressource) {
+        if (config[ressourceName][field][mode].includes(role)) {
+          filteredData[field] = ressource[field];
+        }
+      }
+      return filteredData;
+    };
   };
 };
