b46e193deb3083a71b845b8bce0ade679fe3cd92
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-01 16:37:38 +0200

    Implement fields authorization filtering

--- services/fieldsAuthorization.service.js
+++ services/fieldsAuthorization.service.js
@@ -1,3 +1,11 @@
-module.exports = function fieldsAutorization() {
-  return null;
+module.exports = function fieldsAutorization(config) {
+  return (ressourceName) => (role) => (mode) => (ressource) => {
+    const filteredData = {};
+    for (let field in ressource) {
+      if (config[ressourceName][field][mode].includes(role)) {
+        filteredData[field] = ressource[field];
+      }
+    }
+    return filteredData;
+  };
 };
