faf8cb71031cda6912580e71eb31e3025a6080f3
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-01 17:13:24 +0200

    Implement throwing error on unconfigured field

--- services/fieldsAuthorization.service.js
+++ services/fieldsAuthorization.service.js
@@ -8,7 +8,11 @@ module.exports = function fieldsAutorization(config) {
     return (role) => (mode) => (ressource) => {
       const filteredData = {};
       for (let field in ressource) {
-        const authorized = config[ressourceName][field][mode];
+        const fieldConfiguration = config[ressourceName][field];
+        if (fieldConfiguration == null) {
+          throw new Error(`No configuration found for field '${field}'`);
+        }
+        const authorized = fieldConfiguration[mode];
         if (authorized == null) {
           throw new Error(`No configuration found for mode '${mode}'`);
         }
