d7c4e6731e7a96ed0edbea235bd8172870068ae7
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-07 15:04:35 +0200

    Register the delete user route

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const controller = require("../controllers/user.controller");
 const localAuthentication = require("../middelwares/localAuthentication");
+const authentication = require("../middelwares/jwtAutentication");
 
 router.post("/", controller.addUser);
 router.post("/login", localAuthentication, controller.login);
 router.get("/:id", controller.getUser);
+router.delete("/:id", authentication, controller.deleteUser);
 module.exports = router;
