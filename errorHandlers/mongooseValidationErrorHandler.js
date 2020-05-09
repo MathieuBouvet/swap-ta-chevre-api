c9f04bec73466ef996a4320adae84f013cf0ebd2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-09 19:50:43 +0200

    Extract only relevant fields from the error

--- errorHandlers/mongooseValidationErrorHandler.js
+++ errorHandlers/mongooseValidationErrorHandler.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+
+function filterErrorDetails(mongooseError) {
+  let filtered = {};
+  for (let key in mongooseError) {
+    const { kind, message, name } = mongooseError[key];
+    filtered[key] = { kind, message, name };
+  }
+  return filtered;
+}
 module.exports = {
   handledErrorType: mongoose.Error.ValidationError,
   handler: (err, req, res) => {
     res.status(400).json({
       httpStatus: 400,
       httpMessage: "Bad Request",
-      errorDetails: err.errors,
+      errorDetails: filterErrorDetails(err.errors),
     });
   },
 };
