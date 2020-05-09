04e5b4b2ad8daad7ec7bce81915fd737ff1dd2b6
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-09 19:42:10 +0200

    Remove useless describe block

--- __tests__/models.tests/user.model.int.test.js
+++ __tests__/models.tests/user.model.int.test.js
@@ -6,17 +6,15 @@ const User = require("../../models/user.model");
 const dbUtils = require("../../test-utils/dbUtils");
 const { userSeedWithPassword } = require("../../test-utils/userSeedData");
 
-describe("password comparison", () => {
-  beforeAll(dbUtils.setup);
-  afterAll(dbUtils.teardown);
+beforeAll(dbUtils.setup);
+afterAll(dbUtils.teardown);
 
-  it("should check given password", async () => {
-    const user = new User(userSeedWithPassword);
-    await user.save();
+it("should check given password", async () => {
+  const user = new User(userSeedWithPassword);
+  await user.save();
 
-    const checkPassword = await user.isMyPassword("test-user-password");
-    const checkPassword2 = await user.isMyPassword("notmypassword");
-    expect(checkPassword).toBe(true);
-    expect(checkPassword2).toBe(false);
-  });
+  const checkPassword = await user.isMyPassword("test-user-password");
+  const checkPassword2 = await user.isMyPassword("notmypassword");
+  expect(checkPassword).toBe(true);
+  expect(checkPassword2).toBe(false);
 });
