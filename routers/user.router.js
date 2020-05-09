b2dc78817d7fea0a76e8e7ed5b2c8dcd2c29ef27
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-09 23:43:00 +0200

    Update auth middleware usage

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const controller = require("../controllers/user.controller");
-const localAuthentication = require("../middelwares/localAuthentication");
-const authentication = require("../middelwares/jwtAutentication");
+const authentication = require("../middelwares/authentication");
 
 router.post("/", controller.addUser);
-router.post("/login", localAuthentication, controller.login);
+router.post("/login", authentication({ strategy: "local" }), controller.login);
 router.get("/:id", controller.getUser);
-router.delete("/:id", authentication, controller.deleteUser);
+router.delete("/:id", authentication(), controller.deleteUser);
 module.exports = router;
