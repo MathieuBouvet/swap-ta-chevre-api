d5d730f06ddd2ff47692b827e3524a5a8df4bc90
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-09 15:20:29 +0200

    Use the userSeed util

--- __tests__/services.tests/user.service.int.test.js
+++ __tests__/services.tests/user.service.int.test.js
@@ -11,23 +11,19 @@ const User = require("../../models/user.model");
 const InvalidArgumentError = require("../../utils/errors/InvalidArgumentError");
 
 const dbUtils = require("../../test-utils/dbUtils");
+const { userSeedWithPassword } = require("../../test-utils/userSeedData");
 
 beforeAll(dbUtils.setup);
 
 afterAll(dbUtils.teardown);
 
-const userData = {
-  username: "thetestuser",
-  password: "theuserpassword",
-  mail: "theusermail@test-mail.com",
-};
-
 describe("User creation service", () => {
-  beforeEach(async () => {
+  afterEach(async () => {
     await User.deleteMany({});
   });
+
   it("should return a user", async () => {
-    const userDocument = await createUser(userData);
+    const userDocument = await createUser(userSeedWithPassword);
     expect(userDocument).toBeInstanceOf(User);
   });
 
@@ -41,25 +37,24 @@ describe("User creation service", () => {
   });
 
   it("should save user in database", async () => {
-    const userDocument = await createUser(userData);
+    const userDocument = await createUser(userSeedWithPassword);
     const userInDataBase = await User.findById(userDocument._id);
-    expect(userInDataBase.username).toBe(userData.username);
-    expect(userInDataBase.mail).toBe(userData.mail);
+    expect(userDocument.toObject()).toEqual(userInDataBase.toObject());
   });
 
   it("should hash the password", async () => {
-    const userDocument = await createUser(userData);
+    const userDocument = await createUser(userSeedWithPassword);
     const userInDataBase = await User.findById(userDocument._id);
-    expect(userInDataBase.password).not.toBe(userData.password);
+    expect(userInDataBase.password).not.toBe(userSeedWithPassword.password);
     const match = await bcrypt.compare(
-      userData.password,
+      userSeedWithPassword.password,
       userInDataBase.password
     );
     expect(match).toBe(true);
   });
 
   it("should not hash password if not modified", async () => {
-    const userDocument = await createUser(userData);
+    const userDocument = await createUser(userSeedWithPassword);
     const userInDb = await User.findById(userDocument._id);
     const resavedUser = await userInDb.save();
 
@@ -67,8 +62,8 @@ describe("User creation service", () => {
   });
 
   it("should not allow duplicate username", async () => {
-    await createUser(userData);
-    await expect(createUser(userData)).rejects.toThrow(
+    await createUser(userSeedWithPassword);
+    await expect(createUser(userSeedWithPassword)).rejects.toThrow(
       mongoose.Error.ValidationError
     );
   });
@@ -77,22 +72,18 @@ describe("User creation service", () => {
 describe("User finder by id service", () => {
   let seededUser = null;
   beforeAll(async () => {
-    seededUser = await new User({
-      username: "test-user-username",
-      password: "test-user-password",
-      mail: "test-user-mail@test-mail.com",
-    }).save();
+    seededUser = await new User(userSeedWithPassword).save();
   });
   afterAll(async () => {
     await User.deleteMany({});
   });
   it("should return user with given id", async () => {
     const searchedUser = await findUserById(seededUser._id);
-    expect(searchedUser.toObject()).toMatchObject(seededUser.toObject());
+    expect(searchedUser.toObject()).toEqual(seededUser.toObject());
   });
   it("should allow fields selection", async () => {
     const searchedUser = await findUserById(seededUser._id, "username -_id");
-    expect(searchedUser.toObject()).toEqual({ username: "test-user-username" });
+    expect(searchedUser.toObject()).toEqual({ username: "test-user" });
   });
   it("should return null when id is not found", async () => {
     const notFound = await findUserById(mongoose.Types.ObjectId());
@@ -111,22 +102,18 @@ describe("User finder by id service", () => {
 describe("User finder by name service", () => {
   let seededUser = null;
   beforeAll(async () => {
-    seededUser = await new User({
-      username: "test-user-username",
-      password: "test-user-password",
-      mail: "test-user-mail@test-mail.com",
-    }).save();
+    seededUser = await new User(userSeedWithPassword).save();
   });
   it("Should return user with given name", async () => {
     const searchedUser = await findUserByName(seededUser.username);
-    expect(searchedUser.toObject()).toMatchObject(seededUser.toObject());
+    expect(searchedUser.toObject()).toEqual(seededUser.toObject());
   });
   it("should allow fields selection", async () => {
     const searchedUser = await findUserByName(
       seededUser.username,
       "username -_id"
     );
-    expect(searchedUser.toObject()).toEqual({ username: "test-user-username" });
+    expect(searchedUser.toObject()).toEqual({ username: "test-user" });
   });
   it("should return null when username does not exist", async () => {
     const notFound = await findUserByName("username-does-not-exist");
