547f418ca48b31fe3dfb725eff33d15752f2ed4f
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-10 13:39:07 +0200

    Implement withArgsFlexibility

--- utils/withArgsFlexibility.js
+++ utils/withArgsFlexibility.js
@@ -1,3 +1,9 @@
-const withArgsFlexibility = (func) => null;
+const withArgsFlexibility = (func) => (...args) =>
+  args.reduce((partial, arg) => {
+    if (typeof partial !== "function") {
+      return partial;
+    }
+    return partial(arg);
+  }, func);
 
 module.exports = withArgsFlexibility;
