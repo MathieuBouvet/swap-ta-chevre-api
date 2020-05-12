066fdff4e8be68ecbda0f0ec5db5799bd69fb32d
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-05-12 16:32:07 +0200

    Remove passing a configuration object
    It is now read directly from the function scope

--- services/fieldsAuthorization.service.js
+++ services/fieldsAuthorization.service.js
@@ -31,29 +31,25 @@ const config = {
   },
 };
 
-module.exports = function fieldsAutorization(config = configuration) {
-  return (ressourceName) => {
-    if (config[ressourceName] == null) {
-      throw new Error(
-        `No configuration found for ressource '${ressourceName}'`
-      );
-    }
-    return (role) => (mode) => (ressource) => {
-      const filteredData = {};
-      for (let field in ressource) {
-        const fieldConfiguration = config[ressourceName][field];
-        if (fieldConfiguration == null) {
-          throw new Error(`No configuration found for field '${field}'`);
-        }
-        const authorized = fieldConfiguration[mode];
-        if (authorized == null) {
-          throw new Error(`No configuration found for mode '${mode}'`);
-        }
-        if (authorized.includes(role)) {
-          filteredData[field] = ressource[field];
-        }
+module.exports = function fieldsAutorization(ressourceName) {
+  if (config[ressourceName] == null) {
+    throw new Error(`No configuration found for ressource '${ressourceName}'`);
+  }
+  return (role) => (mode) => (ressource) => {
+    const filteredData = {};
+    for (let field in ressource) {
+      const fieldConfiguration = config[ressourceName][field];
+      if (fieldConfiguration == null) {
+        throw new Error(`No configuration found for field '${field}'`);
       }
-      return filteredData;
-    };
+      const authorized = fieldConfiguration[mode];
+      if (authorized == null) {
+        throw new Error(`No configuration found for mode '${mode}'`);
+      }
+      if (authorized.includes(role)) {
+        filteredData[field] = ressource[field];
+      }
+    }
+    return filteredData;
   };
 };
